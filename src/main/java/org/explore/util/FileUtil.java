f06989b08a63ed5c58aadda432699bd34d38ba3d
neil <lirui10093622@163.com>
2019-03-23 23:01:22 +0800

    添加读文件的方法

--- src/main/java/org/explore/util/FileUtil.java
+++ src/main/java/org/explore/util/FileUtil.java
@@ -2,10 +2,14 @@ package org.explore.util;
 
 import lombok.extern.slf4j.Slf4j;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.charset.Charset;
 
 /**
  * @author neil
@@ -21,17 +25,56 @@ public class FileUtil {
         try {
             out = new FileOutputStream(file);
             byte[] bytes = new byte[100];
-            while (in.read(bytes) != -1) {
-                out.write(bytes);
+            int count;
+            while ((count = in.read(bytes)) >= 0) {
+                out.write(bytes, 0, count);
             }
             out.flush();
         } catch (Exception e) {
-            log.error("写文章异常", e);
+            log.error("写文件异常", e);
         } finally {
             if (out != null) {
                 try {
                     out.close();
                 } catch (IOException e) {
+                    log.error("关闭资源异常", e);
+                }
+            }
+        }
+    }
+
+    public static void write(String content, Charset charset, String filePath) {
+        write(new ByteArrayInputStream(content.getBytes(charset)), filePath);
+    }
+
+    public static String read(String filePath, Charset charset) {
+        File file = new File(filePath);
+        FileInputStream in = null;
+        ByteArrayOutputStream out = null;
+        try {
+            in = new FileInputStream(file);
+            out = new ByteArrayOutputStream();
+            byte[] bytes = new byte[100];
+            int count;
+            while ((count = in.read(bytes)) >= 0) {
+                out.write(bytes, 0, count);
+            }
+            return new String(out.toByteArray(), charset);
+        } catch (Exception e) {
+            log.error("写文件异常", e);
+            return "";
+        } finally {
+            if (in != null) {
+                try {
+                    in.close();
+                } catch (Exception e) {
+                    log.error("关闭资源异常", e);
+                }
+            }
+            if (out != null) {
+                try {
+                    out.close();
+                } catch (Exception e) {
                     log.error("关闭资源异常", e);
                 }
             }
--- src/test/java/org/explore/util/FileUtilTest.java
+++ src/test/java/org/explore/util/FileUtilTest.java
@@ -1,18 +1,28 @@
 package org.explore.util;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 
 import java.io.ByteArrayInputStream;
+import java.nio.charset.Charset;
 
 /**
  * @author neil
  * @email lirui10093622@163.com
  * @time 2019-01-07 18:17:11
  */
+@Slf4j
 public class FileUtilTest {
 
     @Test
     public void write() {
         FileUtil.write(new ByteArrayInputStream("文件内容".getBytes()), "/tmp/a.txt");
+        FileUtil.write("文件内容", Charset.forName("UTF-8"), "/tmp/b.txt");
+    }
+
+    @Test
+    public void read() {
+        String content = FileUtil.read("/Users/neil/Desktop/14_1553336983694_dOf3eb4a.txt", Charset.forName("UTF-8"));
+        log.info("文章内容: {}", content);
     }
 }
